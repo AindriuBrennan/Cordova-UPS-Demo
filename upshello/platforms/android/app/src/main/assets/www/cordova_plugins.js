aac490ebfeaa1987e8ec8c302d7c8925a9bb79f4
Andrew Brennan <andrew-brennan@live.com>
2020-02-03 12:17:00 +0000

    firt commit of demo project

---
+++ upshello/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "phonegap-plugin-push.PushNotification",
+      "file": "plugins/phonegap-plugin-push/www/push.js",
+      "pluginId": "phonegap-plugin-push",
+      "clobbers": [
+        "PushNotification"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-support-google-services": "1.3.2",
+    "phonegap-plugin-multidex": "1.0.0",
+    "phonegap-plugin-push": "2.3.0",
+    "cordova-plugin-aerogear-push": "2.8.0-rc1",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-webpack": "0.4.7"
+  };
+});
---
+++ upshello/src/index.js
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { PushRegistration } from '@aerogear/push'
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    this.registerToUPS();
+
+    PushRegistration.onMessageReceived((notification => {
+      console.log('Received push notification: ', notification.message);
+    }));
+  },
+
+  registerToUPS: () => {
+    console.log('Registering...');
+    new PushRegistration({
+      url: 'http://localhost:9999/',
+      android: {
+        senderID: '211412359469',
+        variantID: 'e828116a-c71b-44cb-84f1-7d13ce42c9b8',
+        variantSecret: 'c07e0a10-b274-45f1-b7f5-01dc653d6e44'
+      }
+    })
+    .register()
+    .then(() => {
+      console.log('Registered!');
+    })
+    .catch(error => {
+      console.log('Failed: ', error.message, JSON.stringify(error))
+    });
+  }
+};
