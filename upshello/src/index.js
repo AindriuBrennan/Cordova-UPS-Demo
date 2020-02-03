cb4615f48aaafd316bf946c885d8074bbd87fd42
Andrew Brennan <andrew-brennan@live.com>
2020-02-03 12:44:57 +0000

    added ondDeviceReady function to src/injex.js

--- upshello/src/index.js
+++ upshello/src/index.js
@@ -25,6 +25,15 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+  },
+
+
   // Update DOM on a Received Event
   receivedEvent: function(id) {
     var parentElement = document.getElementById(id);
